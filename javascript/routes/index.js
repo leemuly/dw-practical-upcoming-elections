78f11b1cc20499b29d7fde71d9a5fd1fcab064b0
Marigrace Leeman <leemuly@gmail.com>
2020-02-10 14:07:19 -0500

    reorganized utils

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var us_states = require('../us_state.js');
-var { OCDIDGen, getElections }= require('../jsUtils');
+var us_states = require('../utils/us_state.js');
+var { OCDIDGen, getElections }= require('../utils/jsUtils');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
